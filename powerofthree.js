d1eeaed0f8e425ea3116c29913a50ec78435c208
kevindai777 <kevin.dai.2451@gmail.com>
2020-08-05 23:52:33 -0400

    solutions

---
+++ powerofthree.js
@@ -0,0 +1,10 @@
+//
+
+
+//O(1) solution that uses math to derive the answer
+
+//In our case, x = 3 ^ i
+//i = log3(x)
+//i = log10(x) / log10(3)
+//'% 1' is to check if the decimal is 0
+return (Math.log10(n) / Math.log10(3)) % 1 == 0
